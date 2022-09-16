f1e67684cce2a0e4a28ab43d1953aff55df410b8
Q1 Blue <105375894+q1blue@users.noreply.github.com>
2022-09-17 02:20:16 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Login/>
+    <particles-bg type="random" :canvas="{backgroundColor:'#888'}" :bg="true"/>
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login";
+import { ParticlesBg } from "particles-bg-vue";
+export default {
+  name: "App",
+  components: {
+    Login,
+    ParticlesBg
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="centered-container">
+    <md-content class="md-elevation-3">
+      <div class="title">
+        <img src="https://github.com/lindelof/particles-bg-vue/raw/master/images/logo.png?raw=true">
+        <div class="md-title">particles-bg-vue</div>
+        <div class="md-body-1">A Vue.js particles animation background component</div>
+      </div>
+
+      <div class="form">
+        <md-field>
+          <label>E-mail</label>
+          <md-input v-model="login.email" autofocus></md-input>
+        </md-field>
+
+        <md-field md-has-password>
+          <label>Password</label>
+          <md-input v-model="login.password" type="password"></md-input>
+        </md-field>
+      </div>
+
+      <div class="actions md-layout md-alignment-center-space-between">
+        <a href="/resetpassword">Reset password</a>
+        <md-button class="md-raised md-primary" @click="auth">Log in</md-button>
+      </div>
+
+      <div class="loading-overlay" v-if="loading">
+        <md-progress-spinner md-mode="indeterminate" :md-stroke="2"></md-progress-spinner>
+      </div>
+    </md-content>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      loading: false,
+      login: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    auth() {
+      // your code to login user
+      // this is only for example of loading
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 5000);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.centered-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  .title {
+    text-align: center;
+    margin-bottom: 30px;
+    img {
+      margin-bottom: 16px;
+      max-width: 300px;
+    }
+  }
+  .actions {
+    .md-button {
+      margin: 0;
+    }
+  }
+  .form {
+    margin-bottom: 60px;
+  }
+  .md-content {
+    z-index: 1;
+    padding: 40px;
+    width: 100%;
+    max-width: 400px;
+    position: relative;
+  }
+  .loading-overlay {
+    z-index: 10;
+    top: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.9);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
